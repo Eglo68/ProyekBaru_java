1f49ac43d416063f83cb289d66d03ba1697ab89b
Eglo68 <damanikvavian@gmail.com>
2025-08-06 09:54:30 +0700

    ProyekBaru.java

--- src/proyekbaru/ProyekBaru.java
+++ src/proyekbaru/ProyekBaru.java
@@ -53,15 +53,29 @@ public class ProyekBaru {
 //        JOptionPane.showInputDialog("Masukan Nama:");
 //        System.out.println("Nama : "+nama);
 
-        String a =
-        JOptionPane.showInputDialog("Masukan nilai untuk variabel a : ");
+        // String a =
+        // JOptionPane.showInputDialog("Masukan nilai untuk variabel a : ");
+        
+        // String b =
+        // JOptionPane.showInputDialog("Masukan nilai untuk variabel b : ");
+        
+        // System.out.println("Hasil :" +
+        // (Integer.parseInt(a) *
+        // Integer.parseInt(b)));
 
-        String b =
-        JOptionPane.showInputDialog("Masukan nilai untuk variabel b : ");
+        // String nama = "Budi";
+        // String jurusan = "Sistem Informasi";
+        // JOptionPane.showMessageDialog(null, "Halo " + nama+ ",jurusan Kamu " + jurusan);
 
-        System.out.println("Hasil :" +
-        (Integer.parseInt(a) *
-        Integer.parseInt(b)));
+        // JOptionPane.showMessageDialog(null, "Sebuah Informasi.","Informasi",JOptionPane.INFORMATION_MESSAGE);
+
+        // JOptionPane.showMessageDialog(null, "Sebuah Error !","Error",JOptionPane.ERROR_MESSAGE);
+
+        // JOptionPane.showMessageDialog(null, "Sebuah Peringatan.","Peringatan",JOptionPane.WARNING_MESSAGE);
+
+        // JOptionPane.showMessageDialog(null, "Sebuah Pertanyaan.", "Pertanyaan",JOptionPane.QUESTION_MESSAGE);
+
+        // JOptionPane.showMessageDialog(null, "Sebuah Plain Message.","Plain Message",JOptionPane.PLAIN_MESSAGE);
         
     }
     
